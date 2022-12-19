88fa7a9be54d1e80ca4f022e40c6f9f890421c09
Giopav <43653262+Giopav@users.noreply.github.com>
2022-12-19 17:02:50 +0100

    Initial Commit

---
+++ src/main/java/it/giopav/enchantsx/EnchantsX.java
@@ -0,0 +1,12 @@
+package it.giopav.enchantsx;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnchantsX extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+}
